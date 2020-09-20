1cc59681c902d16e7e2155680c2242ac42d30df0
ShulepovDen <shulepovdenis@mail.ru>
2020-09-20 16:16:57 +0300

    as

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 var day,hours,minutes,seconds;
 function init() {
-day=new Date
-hours=day.getHours();
-minutes=day.getMinutes();
-seconds=day.getSeconds();
+day = new Date
+hours = day.getHours();
+minutes = day.getMinutes();
+seconds = day.getSeconds();
 clock();
 };
 function clock() {
-seconds++;
-if(seconds===60){
-  seconds=0;
-  minutes++
-  if(minutes===60){
-    minutes=0;
-    hours++
-    if(hours===24){
-      hours=0
+seconds ++;
+if(seconds === 60){
+  seconds = 0;
+  minutes ++
+  if(minutes === 60){
+    minutes= 0;
+    hours ++
+    if(hours === 24){
+      hours = 0
     }
   }
 }
@@ -26,10 +26,10 @@ setTimeout(clock,1000);
 };
 
 function changeTimeElement(id,value){
-if(value<10){
-  value='0'+value
+if(value < 10){
+  value = '0' + value
 }
 document.getElementById(id).innerHTML=value;
 };
 
-window.onload=init;
+window.onload = init;
