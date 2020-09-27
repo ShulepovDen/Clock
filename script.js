7d242df62cdf90191247378839d7529feae2bf76
ShulepovDen <shulepovdenis@mail.ru>
2020-09-27 22:57:43 +0300

    Prettier was used

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-var day,hours,minutes,seconds;
+var day, hours, minutes, seconds;
 function init() {
-  day = new Date
+  day = new Date();
   hours = day.getHours();
   minutes = day.getMinutes();
   seconds = day.getSeconds();
   clock();
-};
+}
 function clock() {
-  seconds ++;
-  if(seconds === 60){
+  seconds++;
+  if (seconds === 60) {
     seconds = 0;
-    minutes ++
-    if(minutes === 60){
-      minutes= 0;
-      hours ++
-      if(hours === 24){
-        hours = 0
+    minutes++;
+    if (minutes === 60) {
+      minutes = 0;
+      hours++;
+      if (hours === 24) {
+        hours = 0;
       }
     }
   }
-  changeTimeElement('sec',seconds);
-  changeTimeElement('min',minutes);
-  changeTimeElement('hr',hours);
-  setTimeout(clock,1000);
-};
+  changeTimeElement("sec", seconds);
+  changeTimeElement("min", minutes);
+  changeTimeElement("hr", hours);
+  setTimeout(clock, 1000);
+}
 
-function changeTimeElement(id,value){
-  if(value < 10){
-    value = '0' + value
+function changeTimeElement(id, value) {
+  if (value < 10) {
+    value = "0" + value;
   }
-  document.getElementById(id).innerHTML=value;
-};
+  document.getElementById(id).innerHTML = value;
+}
 
 window.onload = init;
